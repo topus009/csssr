f5c096345a046e27548fc50977a9980e5696d32a
topus009 <sj_89@mail.ru>
2018-11-11 19:24:51 +0300

    Updates

---
+++ precache-manifest.f09ece53358ccc0765f03a1cc4446c5c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "6fd7dc86b030d4f2f1ef",
+    "url": "/csssr/static/js/main.6fd7dc86.chunk.js"
+  },
+  {
+    "revision": "b36c5939986e1f29f72d",
+    "url": "/csssr/static/js/1.b36c5939.chunk.js"
+  },
+  {
+    "revision": "7c2fc9cc48464c444418",
+    "url": "/csssr/static/js/runtime~main.7c2fc9cc.js"
+  },
+  {
+    "revision": "41ce4fc89446e491331be0dc73c7912c",
+    "url": "/csssr/index.html"
+  }
+];
